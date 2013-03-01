d86bee88630c65b8a1c0fb9255c0d5beaa74d884
dcaruana <david@caruana.co.uk>
2013-03-01 21:12:26 +0000

    investigate idea of sys vs user properties

--- src/main/java/caruana/silverbirch/server/schema/Schema.java
+++ src/main/java/caruana/silverbirch/server/schema/Schema.java
@@ -1,19 +1,24 @@
 package caruana.silverbirch.server.schema;
 
-public interface Schema {
+public interface Schema
+{
 
+    public final static String SYS_NS = "sys";
+    public final static String USR_NS = "usr";
+    
     public final static String DB_ID = ":db/id";
     public final static String DB_IDENT = ":db/ident";
     public final static String DB_FN = ":db/fn";
     public final static String DB_PARTITION_USER = ":db.part/user";
 
-    public static final String SYSTEM_UUID = ":system/uuid";
-    public static final String SYSTEM_UNIQUE_NAME = ":system/unique_name";
+    public static final String SYSTEM_UUID = ":" + SYS_NS + ".core/uuid";
+    public static final String SYSTEM_UNIQUE_NAME = ":" + SYS_NS + ".core/unique_name";
     
-    public static final String ITEM_NAME = ":item/name";
-    public static final String ITEM_ROOT = ":item/root";
-    public static final String ITEM_PARENTS = ":item/parents";
+    public static final String ITEM_NAME = ":" + SYS_NS + ".item/name";
+    public static final String ITEM_ROOT = ":" + SYS_NS + ".item/root";
+    public static final String ITEM_PARENTS = ":" + SYS_NS + ".item/parents";
 
-    public static final String BLOB_LENGTH = ":blob/length";
-    public static final String BLOB_MIMETYPE = ":blob/mimetype";
+    public static final String BLOB_LENGTH = ":" + SYS_NS + ".blob/length";
+    public static final String BLOB_MIMETYPE = ":" + SYS_NS + ".blob/mimetype";
+    
 }
